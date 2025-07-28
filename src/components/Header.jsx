815274ffd6c2a6d2ea46d59977fccd78fbd5a30b
dayneryssss <dashavasileva2003@gmail.com>
2025-07-28 15:36:42 +0300

    First commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { NavLink, useNavigate } from "react-router-dom";
+
+const Header = () => {
+    return (
+        <div>header</div>
+    )
+};
+
+export default Header;
